c1df1a48d394b4ea867349fbe46a2cee69dae0d7
sumit Kumar <55837044+sumitkumar9988@users.noreply.github.com>
2021-02-17 23:24:16 +0530

    add userDetail controller

--- backend/router/authRouter.js
+++ backend/router/authRouter.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const authController=require('./../controller/authController');
+const userController=require('./../controller/userController')
 
 const router=express.Router();
 router.post('/signup',authController.signUp);
@@ -7,6 +8,7 @@ router.post('/login',authController.login);
 router.post('/forgetpassword',authController.forgotPassword);
 router.post('/resetpassword/:token',authController.resetPassword);
 router.post('/changepassword',authController.protect,authController.updatePassword);
+router.route('/').get(authController.protect,userController.userDetail)
 
 
 
