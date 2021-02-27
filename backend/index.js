033e48347a3af4f2f7ec69f7fbf10bc99850f33b
sumit Kumar <55837044+sumitkumar9988@users.noreply.github.com>
2021-02-28 02:25:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,18 @@ mongoose
 app.use(express.json());
 app.use('/api/v1/user', authRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
