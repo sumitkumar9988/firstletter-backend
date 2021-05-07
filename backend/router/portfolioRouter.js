e5e70761deee3a4e0ea3229ed3ae6956f6eba656
sumit Kumar <55837044+sumitkumar9988@users.noreply.github.com>
2021-05-08 02:06:47 +0530

    Update portfolioRouter.js

--- backend/router/portfolioRouter.js
+++ backend/router/portfolioRouter.js
@@ -3,5 +3,17 @@ const portflioController = require('./../controller/portflioController');
 const router = express.Router();
 
 router.get('/profile/:username',portflioController.getDetailsByUsername);
+router.get('/profile/:username/basic', portflioController.getBasicDetails);
+router.get(
+  '/profile/:username/education',
+  portflioController.getEducationDetails
+);
+router.get(
+  '/profile/:username/experience',
+  portflioController.getExperienceDetails
+);
+router.get('/profile/:username/project', portflioController.getProjectDetails);
+router.get('/profile/:username/certificate', portflioController.getCertificate);
+
 
 module.exports = router;
